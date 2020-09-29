9a13f0c890ab7ffe011ead7c039f8f90735f2d9f
Martin Kemp <martinkempsa@gmail.com>
2020-09-29 10:41:00 +0200

    cleaning up

--- getData.py
+++ getData.py
@@ -2,21 +2,16 @@ import requests
 
 r = requests.get('https://reqres.in/api/users')
 
-# print(r.json())
-
 userList = r.json()
 
-# print(userList["data"][0])
-
+# Print users for troubleshooting
 for k in userList["data"]:
     print(k["first_name"] + " " + k["last_name"])
 
 # Create Markdown file
 f = open("userlist.md", "w")
 f.write("# User List\n")
-
 for k in userList["data"]:
     user = ("- " + k["first_name"] + " " + k["last_name"] + "\n")
     f.write(user)
-
 f.close()
