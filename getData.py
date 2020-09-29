68740bb1fa7689fc52c115900889112ee6929cc8
Martin Kemp <martinkempsa@gmail.com>
2020-09-29 09:14:55 +0200

    get users and save in markdown file

---
+++ getData.py
@@ -0,0 +1,22 @@
+import requests
+
+r = requests.get('https://reqres.in/api/users')
+
+# print(r.json())
+
+userList = r.json()
+
+# print(userList["data"][0])
+
+for k in userList["data"]:
+    print(k["first_name"] + " " + k["last_name"])
+
+# Create Markdown file
+f = open("userlist.md", "w")
+f.write("# User List\n")
+
+for k in userList["data"]:
+    user = ("- " + k["first_name"] + " " + k["last_name"] + "\n")
+    f.write(user)
+
+f.close()
